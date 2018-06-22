6c66e3ff844bccc5938f922b774236f06d9bc391
Burakayser <burakayser@msn.com>
2018-06-22 16:50:35 +0300

    Movies between endpoint

--- routes/movie.js
+++ routes/movie.js
@@ -25,6 +25,33 @@ router.get('/', function(req, res,next) {
   });
 });
 
+router.get('/top10', function(req, res,next) {
+  Movie.find({}).limit(10).sort({imdb_score: 1}).then((data) => {
+    res.json(data);
+  })
+  .catch((err) => {
+    res.json(err);
+  });
+});
+
+router.get('/between/:start_year/:end_year', function(req, res, next) {
+  const {start_year, end_year} = req.params;
+
+  const promise = Movie.find({
+    year: { "$gte" : parseInt(start_year) , "$lte" : parseInt(end_year) }
+  });
+  promise.then((movie) => {
+    if(!movie){
+      next({ message: "The movie was not found." , code : 99 });
+      return;
+    }
+    res.json(movie);
+  })
+  .catch((err) => {
+    res.json(err);
+  })
+});
+
 router.get('/:movie_id', function(req, res, next) {
   const promise = Movie.findById(req.params.movie_id);
 
@@ -54,6 +81,22 @@ router.put('/:movie_id', function(req, res, next) {
     res.json(err);
   })
 });
+
+router.delete('/:movie_id', function(req, res, next) {
+  const promise = Movie.findByIdAndRemove(req.params.movie_id);
+
+  promise.then((movie) => {
+    if(!movie){
+      next({ message: "The movie was not found." , code : 99 });
+      return;
+    }
+    res.json({status : 1});
+  })
+  .catch((err) => {
+    res.json(err);
+  })
+});
+
 
 
 
